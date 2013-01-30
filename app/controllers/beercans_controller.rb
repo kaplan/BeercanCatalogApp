09c1da0f31067ece981d80a6762f10bc7304f069
Dave Kaplan <workalicious@gmail.com>
2013-01-30 15:49:10 -0600

    added the form_for helper and left all three versions in the controller file

--- app/controllers/beercans_controller.rb
+++ app/controllers/beercans_controller.rb
@@ -11,18 +11,25 @@ class BeercansController < ApplicationController
   def create
     logger.info "----> CREATE <-----"
 
+    # SUPER SHORT HAND with .create and form_for helper that makes an object for the form data
+    can = Beercan.create(params[:beercan])
+
+    # SHORTER HAND with form_tag helper using params in .create method
     # you can use arguments that will intialize an instance with params for a new model instance. if you use .create it will also .save
-    can = Beercan.create({  :url => params[:url],
-                            :caption => params[:caption],
-                            :brewery => params[:brewery],
-                            :year_brewed => params[:year_brewed]
-                          })
+    # can = Beercan.create({  :url => params[:url],
+    #                         :caption => params[:caption],
+    #                         :brewery => params[:brewery],
+    #                         :year_brewed => params[:year_brewed]
+    #                       })
+
+    # ORIGINAL LONG HAND APPROACH FOR NEW INSTANCE
     # can = Beercan.new
     # can.url = params[:url]
     # can.caption = params[:caption]
     # can.brewery = params[:brewery]
     # can.year_brewed = params[:year_brewed]
     # can.save
+
     redirect_to beercans_url
   end
 
@@ -38,13 +45,17 @@ class BeercansController < ApplicationController
     # logger.info "working on update here:"
     # logger.info params.inspect
     # can = Beercan.find_by_id(params[:id])
-    # logger.info can
-    can = Beercan.find_by_id(params[:id]).update_attributes({
-                                          :url => params[:url],
-                                          :caption => params[:caption],
-                                          :brewery => params[:brewery],
-                                          :year_brewed => params[:year_brewed]
-                                          })
+    logger.info "beercan id on update: #{params["id"]}"
+    # logger.info "beercan object on update here: #{["beercan"]["id"]}"
+    # can = Beercan.find_by_id(params[:beercan])
+    # logger.info "beercan object: #{can}"
+    can = Beercan.find_by_id(params[:id]).update_attributes(params[:beercan])
+    # can = Beercan.find_by_id(params[:id]).update_attributes({
+    #                                       :url => params[:url],
+    #                                       :caption => params[:caption],
+    #                                       :brewery => params[:brewery],
+    #                                       :year_brewed => params[:year_brewed]
+    #                                       })
     # can.url = params[:url]
     # can.caption = params[:caption]
     # can.brewery = params[:brewery]
